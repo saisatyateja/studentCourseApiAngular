b04a6558234d1b9a10ec85de345a3e5d9a8db0bf
saisatyateja <saisatyatejagunisetti@gmail.com>
2023-09-19 00:59:32 +0530

    trying to fix endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://student-srv:8080/course', // Define your development API URL here
+    apiUrl: 'http://studentcoursesrestapi-student-srv-1:8080/course', // Define your development API URL here
   };
   
